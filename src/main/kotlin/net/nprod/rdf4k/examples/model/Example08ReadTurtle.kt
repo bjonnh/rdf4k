748fb29d0336f2ae651b62fb87122d48e7e033f0
Jonathan Bisson <bjonnh-git@bjonnh.net>
2018-12-27 15:45:35 -0600

    Adding example 08 from models

---
+++ src/main/kotlin/net/nprod/rdf4k/examples/model/Example08ReadTurtle.kt
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Eclipse RDF4J contributors.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Distribution License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/org/documents/edl-v10.php.
+ *******************************************************************************/
+package net.nprod.rdf4k.examples.model
+
+import org.eclipse.rdf4j.rio.RDFFormat
+import org.eclipse.rdf4j.rio.Rio
+import java.io.File
+
+import java.io.IOException
+
+/**
+ * RDF Tutorial example 08: Reading a Turtle syntax file to create a Model
+ *
+ * In this example, we show how you can use the Rio Parser/writer toolkit to read files
+ *
+ * @author Jeen Broekstra
+ * @author Jonathan Bisson
+ * @see
+ */
+object Example08ReadTurtle {
+
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+        val filename = "example-data-artists.ttl"
+
+        // read the file 'example-data-artists.ttl' as an InputStream.
+        val input = javaClass.getResource("/$filename").openStream()
+
+        // Rio also accepts a java.io.Reader as input for the parser.
+        val model = Rio.parse(input, "", RDFFormat.TURTLE)
+
+        // To check that we have correctly read the file, let's print out the model to the screen again
+        model.map { statement ->
+            println(statement)
+        }
+    }
+}
